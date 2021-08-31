0bcd46f087253982fae2ace949ffd8d3cf9bc1ea
Irina Ņekrasova <irinanekrasova@outlook.lv>
2021-08-31 21:45:43 +0300

    Jauns fails

---
+++ Lesson1/Newproject31082021/Newproject31082021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Newproject31082021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
